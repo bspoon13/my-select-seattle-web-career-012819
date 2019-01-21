4729e04b76446c4a9edcc0d365d03157cb9dded6
Billy Witherspoon <wwither@gmail.com>
2019-01-21 04:45:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   newArray = []
   while i < collection.length
-    if(yield(collection[i]))
+    if yield(collection[i])
       newArray >> collection[i]
     i += 1
   else 
